f6203f907c17f06ca65735a95aca37403b2fd621
zicros <zilongz@gmail.com>
2019-04-18 12:04:27 -0400

    Adding script to package all NUSPECs

---
+++ pack.ps1
@@ -0,0 +1,26 @@
+$rootLocation = Get-Item .\
+$packageStorePath = Join-Path $rootLocation 'Packages'
+
+# Get the package location or create the folder if it doesn't exist.
+# Get-Item isn't really necessary, but trying to keep everything to use objects
+# rather than strings.
+if (Test-Path $packageStorePath)
+{
+    $packageStoreLocation = Get-Item $packageStorePath
+}
+else
+{
+    $packageStoreLocation = New-Item $packageStorePath
+}
+
+Write-Host "Packages will be stored in $($packageStoreLocation.FullName)"
+
+# Get all the nuspecs found in child paths of the root directory
+$nuspecs = Get-ChildItem -Path $rootLocation -File -Recurse -Filter *.nuspec
+
+foreach ($nuspec in $nuspecs)
+{
+    Write-Host "Building $($nuspec.FullName)"
+    choco pack $nuspec.Fullname --out $packageStoreLocation.FullName
+}
+
